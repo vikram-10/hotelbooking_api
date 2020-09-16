9bfe3741c46eccdb8c3bc54847369aac0a6486b0
vikithespider@gmail.com <vikrambalaji2k@gmail.com>
2020-09-16 17:17:29 +0530

    Added listings

--- app.js
+++ app.js
@@ -5,11 +5,49 @@ var app=express();
 
 app.use(bodyParser.json());
 
+/*
+let room={
+  "id":
+  "seats":
+  "amenitites":
+  "price":
+  "status":
+  "roomName":
+}
+
+let customer={
+  "customerName":
+  "Date":
+  "startTime":
+  "endTime":
+  "roomID":
+}
+
+let correlation={
+  "roomName":
+  "status":
+  "customerName":
+  "Date":
+  "startTime":
+  "endTime"
+}
+
+let correlation1={
+   "customerName":
+   "roomName":
+   "Date":
+   "startTime":
+   "endTime":
+}
+*/
+
 let room=[];
 
 let customer=[];
 
 let correlation=[];
+
+let correlation1=[];
 
 app.get("/room",function(req,res){
     res.json(room);
@@ -22,17 +60,37 @@ app.post("/room",function(req,res){
   });
 });
 
-app.get("/customers",function(req,res){
+app.get("/customer",function(req,res){
   res.json(customer);
 });
 
-app.post("/customers",function(req,res){
+app.post("/customer",function(req,res){
    customer.push(req.body);
    res.json({
        "message": "Customer pushed"
    })
 });
 
+app.get("/listings/:id",function(req,res){
+     let id=req.params.id;
+     room.forEach(element=>{
+       if(element.id==id){
+        customer.forEach(element1=>{
+          if(element.id==element1.roomID){
+            let obj={
+              "roomName":element.roomName,
+              "status":element.status,
+              "customerName":element1.status,
+              "startTime":element1.startTime,
+              "endTime":element1.endTime
+            }
+            correlation.push(obj);
+          }
+        })
+      }
+     });
+     res.json(correlation);
+})
 
 
 app.listen(3000,function(err){
